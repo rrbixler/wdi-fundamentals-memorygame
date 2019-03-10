d3091fa71244812fc204505f88cab00e868a132c
Robert Bixler <rrbixler@Roberts-MacBook-Pro.local>
2019-03-10 09:26:55 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,20 +2,38 @@ console.log("Up and running!");
 
 
 var cards = ["queen", "queen", "king", "king"];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-
 var cardsInPlay = [];
-cardsInPlay.push('cardOne');
-cardsInPlay.push('cardTwo')
 
+cardsInPlay.push('cards[cardId]');
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+//*var cardOne = cards[0];
+//cardsInPlay.push('cardOne');
+//console.log("User flipped " + cardOne);
 
-if (cardsInPlay[0] === cardsInPlay[1]){
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
+//var cardTwo = cards[2];
+//cardsInPlay.push('cardTwo');
+//console.log("User flipped" + cardTwo);
+
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  		console.log("You found a match!");
+	} else {
+  		console.log("Sorry, try again.");
+	}
 }
+
+var flipCard = function(cardId){
+	
+	console.log("User flipped" + cards[cardId])
+
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+flipCard(0);
+flipCard(2);
+
+
